59f0d35cc9de8ede16e9d1daa50b3e29433252bb
Alberto Mansilla <mansilla_alberto@outlook.com>
2021-08-08 14:55:16 -0300

    Updated Challenge Readme

--- 5-desafio/backend/apps/request_loan/tests.py
+++ 5-desafio/backend/apps/request_loan/tests.py
@@ -1,12 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
-# def test_false_is_true():
-#         print("Method: test_false_is_true.")
-#         assert capitalize_string('test') == 'Test'
-
 class ExampleTestClass(TestCase):
 
     def test_false_is_true(self):
         print("Method: test_false_is_true.")
-        self.assertTrue(True)
+        self.assertTrue(False)
