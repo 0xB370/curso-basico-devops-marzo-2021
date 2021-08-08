a4c5e4260790152caef163f5ed8f8591441ab3f7
Alberto Mansilla <mansilla_alberto@outlook.com>
2021-08-07 22:04:02 -0300

    Path Changed

--- 5-desafio/backend/apps/request_loan/tests.py
+++ 5-desafio/backend/apps/request_loan/tests.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 # Create your tests here.
 def test_false_is_true(self):
         print("Method: test_false_is_true.")
-        self.assertTrue(False)
+        self.assertTrue(True)
