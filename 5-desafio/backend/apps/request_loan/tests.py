94ff5bb17490deea7efd508fcbc128b00bebfc04
Alberto Mansilla <mansilla_alberto@outlook.com>
2021-08-07 22:32:10 -0300

    Added Class to TestCase

--- 5-desafio/backend/apps/request_loan/tests.py
+++ 5-desafio/backend/apps/request_loan/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
-def test_false_is_true():
+# def test_false_is_true():
+#         print("Method: test_false_is_true.")
+#         assert capitalize_string('test') == 'Test'
+
+class ExampleTestClass(TestCase):
+
+    def test_false_is_true():
         print("Method: test_false_is_true.")
         assert capitalize_string('test') == 'Test'
